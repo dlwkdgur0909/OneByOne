098cc42517fb4934128eaf01f077296f2265d52a
dlwkdgur0909 <janghyuck0909@gmail.com>
2024-04-14 23:53:27 +0900

    Monster Spawnner 3

--- Assets/Script/MonsterSpawnner.cs
+++ Assets/Script/MonsterSpawnner.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class MonsterSpawnner : MonoBehaviour
 {
-    public GameObject monsterPrefab;
+    public GameObject[] monsterPrefab;
+    private Vector3 exculusionVector;
+
 
     void Start()
     {
+        Vector3 fieldSize = new Vector3(500, 0, 500);
+        Vector3 rectangleSize = new Vector3(80, 0, 80);
+
+        Vector3 rectangleCenter = fieldSize / 2;
+        Vector3 rectangleTopLeft = rectangleCenter - rectangleSize / 2;
+
+        exculusionVector = new Vector3(rectangleTopLeft.x, 0, rectangleTopLeft.z);
+
         StartCoroutine(CO_MonsterSpawn());
     }
 
     void Update()
     {
-       
+
     }
 
     public IEnumerator CO_MonsterSpawn()
     {
-        Instantiate(monsterPrefab);
-        yield return new WaitForSeconds(3f);
+        while (true)
+        {
+            int randomMonster = Random.Range(0, monsterPrefab.Length);
+            Instantiate(monsterPrefab[randomMonster], exculusionVector, Quaternion.identity);
+            yield return new WaitForSeconds(3f);
+        }
     }
 }
