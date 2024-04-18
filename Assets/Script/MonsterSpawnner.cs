fed7ffa10f9c96a87bcca2a3c1196d20e7fbe1eb
dlwkdgur0909 <janghyuck0909@gmail.com>
2024-04-18 20:38:09 +0900

    Monster Spawnner 6

--- Assets/Script/MonsterSpawnner.cs
+++ Assets/Script/MonsterSpawnner.cs
@@ -1,59 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using System.Collections;
 
-public class MonsterSpawnner : MonoBehaviour
+public class MonsterSpawner : MonoBehaviour
 {
-    public GameObject[] monsterPrefab; // 몬스터 프리팹 배열
-    public Vector3 fieldSize; // 전체 필드 크기
-    public Vector3 spawnSize; // 스폰 영역 크기
-    public float noSpawnRegionSize = 150f;
+    public GameObject[] monsterPrefab;
+    public GameObject rangeObj;
+    BoxCollider boxCollider;
+
+    void Awake()
+    {
+        boxCollider = rangeObj.GetComponent<BoxCollider>();
+    }
 
     void Start()
     {
-        fieldSize = new Vector3(500, 0, 500);
-        spawnSize = new Vector3(300, 0, 300);
-
-        StartCoroutine(SpawnMonsters());
+        StartCoroutine(SpawnMonster());
     }
 
-    IEnumerator SpawnMonsters()
+    IEnumerator SpawnMonster()
     {
         while (true)
         {
-            Vector3 randomPosition = GetRandomPositionInSpawnArea();
+            int randomPrefab = Random.Range(0, monsterPrefab.Length);
 
-            int randomMonsterIndex = Random.Range(0, monsterPrefab.Length);
-            GameObject newMonster = Instantiate(monsterPrefab[randomMonsterIndex], randomPosition, Quaternion.identity);
-            // 몬스터의 높이 고려하여 위치 조정
-            newMonster.transform.position += Vector3.up * newMonster.GetComponent<Collider>().bounds.extents.y;
-
-            yield return new WaitForSeconds(3f);
+            Instantiate(monsterPrefab[randomPrefab], Return_RandomPosition(), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
         }
     }
 
-    Vector3 GetRandomPositionInSpawnArea()
+    Vector3 Return_RandomPosition()
     {
-        Vector3 center = transform.position;
-        float halfWigth = spawnSize.x / 2f;
-        float halfDepth = spawnSize.z / 2f;
+        Vector3 originPosition = rangeObj.transform.position;
+        // 콜라이더의 사이즈를 가져오는 bound.size 사용
+        float range_X = boxCollider.bounds.size.x;
+        float range_Z = boxCollider.bounds.size.z;
 
-        Vector3 spawnCenter = center;
-        spawnCenter.y = transform.position.y;
+        range_X = Random.Range((range_X / 2) * -1, range_X / 2);
+        range_Z = Random.Range((range_Z / 2) * -1, range_Z / 2);
+        Vector3 RandomPostion = new Vector3(range_X, 0f, range_Z);
 
-        Vector3 noSpawnRegion = new Vector3(noSpawnRegionSize, 0, noSpawnRegionSize);
-
-
-        Vector3 spawnAreaMin = spawnCenter - spawnSize / 2f + noSpawnRegion / 2f;
-        Vector3 spawnAreaMax = spawnCenter + spawnSize / 2f - noSpawnRegion / 2f;
-
-        Vector3 randomPosition = new Vector3(
-            Random.Range(spawnAreaMin.x, spawnAreaMax.x),
-            transform.position.y,
-            Random.Range(spawnAreaMin.z, spawnAreaMax.z)
-        );
-        return randomPosition;
+        Vector3 respawnPosition = originPosition + RandomPostion;
+        return respawnPosition;
     }
 }
