6dae863d043cf0591e2695bbee4b74754c3a4d56
dlwkdgur0909 <janghyuck0909@gmail.com>
2024-06-10 20:53:44 +0900

    ToDo. UI Modify

--- Assets/Script/Object/Description.cs
+++ Assets/Script/Object/Description.cs
@@ -3,5 +3,46 @@ using UnityEngine.UI;
 
 public class Description : MonoBehaviour
 {
+    private bool isRotated = false;
 
+    private Vector3 originalRot = new Vector3(0, 0, 0);
+    private Vector3 rotatedRot = new Vector3(0, 180, 0);
+
+    private RectTransform rectTransform;
+
+    public GameObject streetLightDes;
+    public GameObject towerDes;
+    public GameObject wireDes;
+    public GameObject damageUpDes;
+    public GameObject reloadSpeedDes;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Input.mousePosition))
+            {
+                OnRotateButtonClick();
+            }
+        }
+    }
+
+    void OnRotateButtonClick()
+    {
+        if (!isRotated)
+        {
+            rectTransform.rotation = Quaternion.Euler(rotatedRot);
+            isRotated = true;
+        }
+        else
+        {
+            rectTransform.rotation = Quaternion.Euler(originalRot);
+            isRotated = false;
+        }
+    }
 }
