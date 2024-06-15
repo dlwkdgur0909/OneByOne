567576f370d9ade2fffe441e7cb7f43dfc081085
dlwkdgur0909 <janghyuck0909@gmail.com>
2024-06-16 00:04:15 +0900

    Title 2

--- Assets/Script/Manager/Sun.cs
+++ Assets/Script/Manager/Sun.cs
@@ -4,11 +4,9 @@ public class Sun : MonoBehaviour
 {
     public Transform sun;
 
-    public float timeInSeconds = 300f;
-
+    public float timeInSeconds = 180f;
     private float elapsedTime = 0f;
 
-    public TMPro.TMP_Text sunAngleText;
     public TMPro.TMP_Text timeText;
 
     void Update()
@@ -16,11 +14,9 @@ public class Sun : MonoBehaviour
         elapsedTime += Time.deltaTime;
 
         float sunAngle = Mathf.Lerp(-170f, 5f, elapsedTime / timeInSeconds);
-
         sun.transform.rotation = Quaternion.Euler(sunAngle, 0f, 0f);
 
-        sunAngleText.text = sunAngle.ToString();
-        timeText.text = elapsedTime.ToString();
+        timeText.text = Mathf.FloorToInt(elapsedTime).ToString();
 
         //시간 다 지남
         if (elapsedTime >= timeInSeconds)
--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
+    public float moveSpeed;
+    public GameObject blackPanel;
+    public GameObject start;
     public GameObject howToPlay;
     public GameObject setting;
     public GameObject credit;
@@ -64,7 +67,14 @@ public class UIManager : MonoBehaviour
 
     public void Exit()
     {
+        Debug.Log("Game Quit");
         Application.Quit();
     }
     #endregion
+
+    private void Update()
+    {
+        //게임 시작할때 샤라락 ㅇㅋ?
+        blackPanel.transform.Translate(new Vector3(-100f, 0, 0) * moveSpeed * Time.deltaTime); 
+    }
 }
